637ef67d04d20e500d2eaab85d3a935c697efaa0
Max Teicheira <Max.Teicheira@gmail.com>
2013-04-22 16:44:00 -0700

    More tests

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Charizard
 Some of my scripts for Charizard
 
 Server for Chrome Extensions
-============================
+----------------------------
+
+json_server.py
+
+
--- json_server.py
+++ json_server.py
@@ -1,12 +1,24 @@
 import tornado.ioloop
 import tornado.web
 
+import transmissionrpc as tr
+
+
+
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello World")
         
     def post(self):
-        print self.request.arguments["data"]
+        print self.request.arguments
+        client = tr.Client()
+            try:
+            torrent = client.add_torrent()
+        except:
+            torrent = client.get_torrents([])
+
 
 application = tornado.web.Application([
     (r"/" ,MainHandler),
